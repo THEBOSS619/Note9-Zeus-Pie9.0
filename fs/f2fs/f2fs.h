a51ce91922518722aaec5110dcf2b1b46d99e4da
Chao Yu <yuchao0@huawei.com>
2019-08-15 19:45:34 +0800

    f2fs: use wrapped IS_SWAPFILE()
    Just cleanup, no logic change.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -3725,7 +3725,7 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 			return true;
 	}
 	if (is_sbi_flag_set(F2FS_I_SB(inode), SBI_CP_DISABLED) &&
-					!(inode->i_flags & S_SWAPFILE))
+					!IS_SWAPFILE(inode))
 		return true;
 
 	return false;
