becf7451fd23016580383de45a465895dea6740b
THEBOSS619 <ehabhani59@gmail.com>
2019-09-13 04:45:57 +0300

    Make little cores to 2Ghz rather than 1.95GHz

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1066,7 +1066,7 @@ static int init_dm(struct exynos_cpufreq_domain *domain,
 	return register_exynos_dm_freq_scaler(domain->dm_type, dm_scaler);
 }
 
-static unsigned long arg_cpu_max_c1 = 1950000;
+static unsigned long arg_cpu_max_c1 = 2002000;
 
 static int __init cpufreq_read_cpu_max_c1(char *cpu_max_c1)
 {
